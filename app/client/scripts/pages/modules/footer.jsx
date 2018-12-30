2c0f3be89529954384e91f5fdb109a3247bbb160
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2018-12-30 23:10:57 +0800

    Add position and group rule
    增加站位表，並依據站位表排序隊伍，以及點擊角色圖示有增加、刪除角色效果。

--- app/client/scripts/pages/modules/footer.jsx
+++ app/client/scripts/pages/modules/footer.jsx
@@ -4,12 +4,15 @@ import React from "react";
 import Team from "pages/modules/team";
 // Utils
 import {paddingZero} from "./utils";
+// Environemt enum
+import {character} from "conf/position.json";
 
 export default class Footer extends React.PureComponent {
     constructor() {
         super();
         this.team = null;
-        this.iconSize = 56;
+        this.characterPositionList = character;
+        this.targetGroup = [];
     }
     chooseATeam() {
         console.log("> Choose A Team: ", this.team.members());
@@ -29,18 +32,51 @@ export default class Footer extends React.PureComponent {
         const event = new CustomEvent("groupTeam", {detail: info});
         window.dispatchEvent(event);
     }
-    chooseTarget(index) {
-        console.log("> Choose : ", index);
-        this.team.add(paddingZero(index, 3));
+    chooseTarget(cid) {
+        console.log("> Choose : ", cid, this.characterPositionList.indexOf(cid), this.targetGroup.indexOf(cid));
+        if (this.targetGroup.length < 5 || (this.targetGroup.length === 5 && this.targetGroup.indexOf(cid) >= 0)) {
+            // character-id exist in group, remove this id, not exist push in.
+            this.removeExistTarget(cid);
+            //
+            this.sortTargetGroup();
+            //
+            const characterStr = this.generateTeamList();
+            this.team.add(characterStr);
+        }
+    }
+    removeExistTarget(cid) {
+        if (this.targetGroup.indexOf(cid) < 0) {
+            this.targetGroup.push(cid);
+        } else {
+            this.targetGroup.splice(this.targetGroup.indexOf(cid), 1);
+        }
+    }
+    sortTargetGroup() {
+        this.targetGroup.sort((cid1, cid2) => {
+            console.log(this.characterPositionList.indexOf(cid1), this.characterPositionList.indexOf(cid2));
+            return this.characterPositionList.indexOf(cid1) - this.characterPositionList.indexOf(cid2);
+        });
+    }
+    generateTeamList() {
+        const list = [];
+        for (let count = 0; count < 5; count += 1) {
+            if (count < this.targetGroup.length) {
+                list.push(paddingZero(this.targetGroup[count], 3));
+            } else {
+                list.push(paddingZero(0, 3));
+            }
+        }
+        console.log(list);
+        return list.reverse();
     }
     renderIcon() {
         const icons = [];
-        for (let count = 1; count <= this.iconSize; count += 1) {
-            const iconPath = `./icons/${paddingZero(count, 3)}.png`;
-            icons.push(<img src={iconPath} onClick={
-              () => { this.chooseTarget(count); }
+        this.characterPositionList.forEach((cid, index) => {
+            const iconPath = `./icons/${paddingZero(cid, 3)}.png`;
+            icons.push(<img key={`character-${index}`} src={iconPath} onClick={
+              () => { this.chooseTarget(cid); }
             }/>);
-        }
+        });
         return icons;
     }
     render() {
